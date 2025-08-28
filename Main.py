a7c3ad7894b06a86b53ba72a3a24875d6f2f72d0
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-08-28 14:50:00 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -75,6 +75,7 @@ if in_file:
     doe_choice = st.selectbox("Select the experiment design method", options=methods, key="select_method")
 
     df_updated, filename = generate_DOE(doe_choice, in_file)
+    st.write(df_updated)
 
     if type(df_updated) != int or type(filename) != int:
         out_file = write_csv(df_updated)
